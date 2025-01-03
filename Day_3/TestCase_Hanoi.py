c620c09c6f5d1b430a2afda591566c90d94982bf
praiseolotu <olioxxayo@gmail.com>
2025-01-03 09:08:56 +0100

    final solution to question 1

--- Day_3/TestCase_Hanoi.py
+++ Day_3/TestCase_Hanoi.py
@@ -1,20 +1,20 @@
 import sys
 import io, unittest
 from unittest import TestCase
-from Tower_Hanoi import TowerHanoi  # Import toh function
+from Tower_Hanoi import TowerHanoi 
 
 class Evaluate(TestCase):
 
-    def assertTohOutput(self, N, expected_output):
+    def assertTohOutput(self, n, expected_output):
         original_stdout = sys.stdout  # Save the original stdout
         sys.stdout = buffer = io.StringIO()  # Redirect stdout to a string buffer
-        actual_moves = TowerHanoi(N, 'A', 'C', 'B')  # Call the toh function
+        actual_moves = TowerHanoi(n, 'A', 'C', 'B')  # Call the toh function
         sys.stdout = original_stdout  # Reset stdout
         actual_output = buffer.getvalue().strip()
         self.assertEqual(actual_output, expected_output.strip(), 
-                         f"Printed output for {N} disks is incorrect.\nExpected:\n{expected_output}\nActual:\n{actual_output}")
-        self.assertEqual(actual_moves, 2**N - 1, 
-                         f"Incorrect number of moves for {N} disks. Expected: {2**N - 1}, Actual: {actual_moves}")
+                         f"Printed output for {n} disks is incorrect.\nExpected:\n{expected_output}\nActual:\n{actual_output}")
+        self.assertEqual(actual_moves, 2**n - 1, 
+              f"Incorrect number of moves for {n} disks. Expected: {2**n - 1}, Actual: {actual_moves}")
 
     def test_toh_1_disk(self):
         expected_output = "move disk 1 from rod A to rod C"
